baa8bef39d818f7c8a7d4501daf6ad683b91fcb4
zhouyulan02282729 <56106516+zhouyulan02282729@users.noreply.github.com>
2019-12-25 16:57:10 +0800

    Add files via upload

---
+++ 凯撒密码-429.py
@@ -0,0 +1,36 @@
+#加密函数
+def encryption(s):
+    m = 1
+    print("加密后:",end='')
+    for p in s:
+        if ord("a") <= ord(p) <=ord("z"):
+            print(chr(ord("a") + (ord(p)-ord("a")+m) % 26),end='')
+            m = m+1
+        elif ord("A") <= ord(p) <=ord("Z"):
+            print(chr(ord("A") + (ord(p)-ord("A")+m) % 26),end='')
+            m = m+1
+        else:
+            print(p,end='')
+
+#解密函数
+def decryption(k):
+    n = 1
+    print("解密后:",end='')
+    for p in k:
+        if ord("a") <= ord(p) <=ord("z"):
+            print(chr(ord("a") + (ord(p)-ord("a")-n) % 26),end='')
+            n = n+1
+        elif ord("A") <= ord(p) <=ord("Z"):
+            print(chr(ord("A") + (ord(p)-ord("A")-n) % 26),end='')
+            n = n+1
+        else:
+            print(p,end='')
+
+s = input("请输入需要加密的信息：")
+k = input("请输入需要解密的信息：")
+encryption(s)
+print('')
+decryption(k)
+
+
+        
